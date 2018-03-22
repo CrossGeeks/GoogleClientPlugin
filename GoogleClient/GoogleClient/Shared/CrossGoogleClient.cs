a6df508c90ea6b45f8e81cb2790a44a4c8ade43c
Luis Pujols <luispujols@hotmail.com>
2018-03-22 13:27:39 -0400

    ADD "Created the base project file structure for the Plugin"

---
+++ GoogleClient/GoogleClient/Shared/CrossGoogleClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Plugin.GoogleClient
+{
+    /// <summary>
+    /// Cross GoogleClient
+    /// </summary>
+    public static class CrossGoogleClient
+    {
+        static Lazy<IGoogleClient> implementation = new Lazy<IGoogleClient>(() => CreateGoogleClient(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Gets if the plugin is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported => implementation.Value == null ? false : true;
+
+        /// <summary>
+        /// Current plugin implementation to use
+        /// </summary>
+        public static IGoogleClient Current
+        {
+            get
+            {
+                IGoogleClient ret = implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IGoogleClient CreateGoogleClient()
+        {
+#if NETSTANDARD1_0 || NETSTANDARD2_0
+            return null;
+#else
+#pragma warning disable IDE0022 // Use expression body for methods
+            return new GoogleClientImplementation();
+#pragma warning restore IDE0022 // Use expression body for methods
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly() =>
+            new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
+    }
+}
---
+++ GoogleClient/GoogleClient/Shared/IGoogleClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.GoogleClient
+{
+    public interface IGoogleClient
+    {
+    }
+}
