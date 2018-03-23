181ee11d2cf15657a5fafb411a08662e2db513ca
Luis Pujols <luispujols@hotmail.com>
2018-03-23 17:56:37 -0400

    ADD "Added the implementation for the IGoogleClientManager, which will be the interface for our client managers in each platform"

--- GoogleClient/GoogleClient/Shared/IGoogleClientManager.cs
+++ GoogleClient/GoogleClient/Shared/IGoogleClientManager.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Plugin.GoogleClient.Shared;
 
 namespace Plugin.GoogleClient
 {
-    public interface IGoogleClientManager
+    public enum GoogleActionStatus
     {
+        Canceled,
+        Unauthorized,
+        Completed,
+        Error
+    }
+
+    class GoogleClientResultEventArgs<T> : EventArgs
+    {
+        public T Data { get; set; }
+        public GoogleActionStatus Status { get; set; }
+        public string Message { get; set; }
+
+        public GoogleClientResultEventArgs(T data, GoogleActionStatus status, string msg = "")
+        {
+            Data = data;
+            Status = status;
+            Message = msg;
+        }
+    }
+
+    class GoogleResponse<T>
+    {
+        public T Data { get; set; }
+        public GoogleActionStatus Status { get; set; }
+        public string Message { get; set; }
+
+        public GoogleResponse(GoogleClientResultEventArgs<T> evtArgs)
+        {
+            Data = evtArgs.Data;
+            Status = evtArgs.Status;
+            Message = evtArgs.Message;
+        }
+
+        public GoogleResponse(T user, GoogleActionStatus status, string msg = "")
+        {
+            Data = user;
+            Status = status;
+            Message = msg;
+        }
+    }
+
+
+    /// <summary>
+    /// Interface for GoogleClientManager
+    /// </summary>
+    interface IGoogleClientManager
+    {
+        event EventHandler<GoogleClientResultEventArgs<GoogleUser>> OnLogin;
+        event EventHandler OnLogout;
+        Task<GoogleResponse<GoogleUser>> LoginAsync();
+        void Logout();
+        bool IsLoggedIn { get; }
     }
 }
