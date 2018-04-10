10ddd5441d2a2ec79144061b813c4d545616bcbd
Luis Pujols <luispujols@hotmail.com>
2018-04-10 14:38:31 -0400

    Added the UserProfile Model, Implemented the LoginPageViewModel,Updated the project package name, and included the Internet Permission.

--- GoogleClient/GoogleClientSample/GoogleClientSample/GoogleClientSample/Models/UserProfile.cs
+++ GoogleClient/GoogleClientSample/GoogleClientSample/GoogleClientSample/Models/UserProfile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 
+// If you wish to create your custom UserProfile just replace the Google Client Google User with your own UserProfile Model
 namespace GoogleClientSample.Models
 {
     public class UserProfile : INotifyPropertyChanged
--- GoogleClient/GoogleClientSample/GoogleClientSample/GoogleClientSample/ViewModels/LoginPageViewModel.cs
+++ GoogleClient/GoogleClientSample/GoogleClientSample/GoogleClientSample/ViewModels/LoginPageViewModel.cs
@@ -1,12 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.ComponentModel;
-using System.Text;
+using System.Diagnostics;
+using System.Windows.Input;
+using Plugin.GoogleClient;
+using Plugin.GoogleClient.Shared;
+using Xamarin.Forms;
 
 namespace GoogleClientSample.ViewModels
 {
     public class LoginPageViewModel : INotifyPropertyChanged
     {
+        public GoogleUser User { get; set; }
+        public string Name
+        {
+            get => User.Name;
+            set => User.Name = value;
+        }
+
+        public string Email
+        {
+            get => User.Email;
+            set => User.Email = value;
+        }
+
+        public Uri Picture
+        {
+            get => User.Picture;
+            set => User.Picture = value;
+        }
+
+        public bool IsLoggedIn { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand LogoutCommand { get; set; }
+        private readonly IGoogleClientManager _googleClientManager;
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(LoginAsync);
+            LogoutCommand = new Command(Logout);
+            _googleClientManager = CrossGoogleClient.Current;
+            IsLoggedIn = false;
+        }
+
+        public void LoginAsync()
+        {
+            _googleClientManager.OnLogin += OnLoginCompleted;
+            _googleClientManager.LoginAsync();
+        }
+
+
+        private void OnLoginCompleted(object sender, GoogleClientResultEventArgs<GoogleUser> loginEventArgs)
+        {
+            if (loginEventArgs.Data != null)
+            {
+                User = loginEventArgs.Data;
+
+                // Log the current User email
+                Debug.WriteLine(User.Email);
+                IsLoggedIn = true;
+            }
+            else
+            {
+                App.Current.MainPage.DisplayAlert("Error", loginEventArgs.Message, "OK");
+            }
+
+            _googleClientManager.OnLogin -= OnLoginCompleted;
+
+        }
+
+        public void Logout()
+        {
+            _googleClientManager.OnLogout += OnLogoutCompleted;
+            _googleClientManager.Logout();
+        }
+
+        private void OnLogoutCompleted(object sender, EventArgs loginEventArgs)
+        {
+            IsLoggedIn = false;
+            User.Email = "Offline";
+            _googleClientManager.OnLogout -= OnLogoutCompleted;
+        }
     }
 }
